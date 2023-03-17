61435a4af81fc7692d6dfc972288f3fb2d557312
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-03-17 23:53:05 +0600

    1001

--- 1001.cpp
+++ 1001.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int main()
+{
+    int A, B;
+    int sum;
+    // cout << "Take first input: \n";
+    cin >> A;
+    // cout << "Take second input: \n";
+    cin >> B;
+    sum = A + B;
+    cout << "X = " << sum <<endl;
+    return 0;
 }
