f117dddfa208b70da663c0459b2e72903bb35a28
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-03-18 00:20:01 +0600

    1004

---
+++ 1004.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    int X;
+    cin >> A;
+    cin >> B;
+    X = A * B;
+    cout << "PROD = " << X <<endl;
+    return 0;
+}
